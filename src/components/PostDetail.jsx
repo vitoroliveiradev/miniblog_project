88086fd7ed3445184075645522d73901acc6643c
vitoroliveiradev <melooliveira2003@gmail.com>
2022-06-29 18:23:40 -0300

    Criando o css do componente que vai exibir cada post (PostDetail)

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 export const PostDetail = ({ post }) => {
   return (
-    <div>
+    <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <p>{post.createdBy}</p>
-      <div>
+      <p className={styles.createdBy}>{post.createdBy}</p>
+      <div className={styles.tags}>
         {post.tagsArray && post.tagsArray.map(tag => (
           <p key={tag}>
             <span>#</span>
