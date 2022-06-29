8d905795afda2ab4b6efdcbf56a5efafea623d9f
vitoroliveiradev <melooliveira2003@gmail.com>
2022-06-29 17:52:15 -0300

    Criando o hook que vai pegar todos os dados do banco de dados, usando todas as funções que o firebase fornece para buscar os dados(query, collection, orderBy, onSnapshot, where))

---
+++ src/hooks/useFetchDocuments.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { 
+  collection, 
+  query, 
+  orderBy, 
+  onSnapshot, 
+  where 
+} from "firebase/firestore";
+
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // Deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  useEffect(() => {
+
+    const loadData = async () => {
+      if(cancelled) return;
+
+      setLoading(true);
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        
+        let q;
+
+        // Busca
+        // dashboard
+
+        // Pegando os dados
+        q = await query(collectionRef, orderBy("createdAt", "desc"));
+
+        // Mapear dados, se alterar um dado, ele vai marcar e ver que tem diferença entre os dados. Vai trazer o dado atualizado. ID vem separado dos dados.
+        await onSnapshot(q, (querySnapshot) => {
+          // Pegando e setando os documentos do banco.
+          setDocuments(
+            querySnapshot.docs.map(doc => ({
+              id: doc.id,
+              ...doc.data();
+            }))
+          )
+
+        })
+        setLoading(false);
+
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+    loadData();
+  }, [docCollection, search, uid, cancelled])
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, [])
+
+  return {
+    documents,
+    loading, 
+    error
+  }
+
+}
