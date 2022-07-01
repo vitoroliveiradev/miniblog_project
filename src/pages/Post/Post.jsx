b09278a894d14dba938f70d009ba4e1929d18b80
vitoroliveiradev <melooliveira2003@gmail.com>
2022-07-01 10:25:30 -0300

    Criando o hook que pega o dado individual de um post. Importando esse hook na página de Post e exibindo os dados do hook em tela

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -2,14 +2,20 @@ import styles from "./Post.module.css";
 
 // Hooks
 import { useParams } from "react-router-dom";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 export const Post = () => {
   const { id } = useParams();
+  const { document: post, loading, error } = useFetchDocument("posts", id);
 
   return (
     <div>
-      <h1>Post</h1>
-      <p>{id}</p>
+      {loading && <p>Carregando post...</p>}
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+        </>
+      )}
     </div>
   )
 }
