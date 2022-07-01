971ec773bee5dff67663ead832d52186fe4fda08
vitoroliveiradev <melooliveira2003@gmail.com>
2022-07-01 10:38:53 -0300

    Estruturando a página de post: Pegando o id do usuário através do hook de contexto. Criando o layout para quando o usuário não tiver nenhum post criado ainda.

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,34 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+
 export const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  // Posts do usuário
+  const posts = [];
+
   return (
-    <h1>Dashboard</h1>
+    <div>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts.</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
+    </div>
   )
 }
